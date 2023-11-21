c38efe132d36d1d38041580998eac19967d7e25f
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2023-11-21 15:53:07 +0500

    add admin module and conf layout

---
+++ modules/admin/assets/AdminAsset.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   21 - 11 2023 15:6:25
+ *   https://ustadev.uz
+ *   https://github.com/JamshidbekAkhlidinov/yii2basic
+ */
+
+namespace app\modules\admin\assets;
+
+use yii\web\AssetBundle;
+
+class AdminAsset extends AssetBundle
+{
+    public $sourcePath = "@vendor/ustadev/velzon-template/src/assets";
+
+    public $css = [
+        'css/bootstrap.min.css',
+        'css/icons.min.css',
+        'css/app.min.css',
+        'css/custom.min.css',
+    ];
+    public $js = [
+        'js/layout.js',
+        'libs/bootstrap/js/bootstrap.bundle.min.js',
+        'libs/simplebar/simplebar.min.js',
+        'libs/node-waves/waves.min.js',
+        'libs/feather-icons/feather.min.js',
+        'js/pages/plugins/lord-icon-2.1.0.js',
+        'js/plugins.js',
+        'js/app.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap5\BootstrapAsset'
+    ];
+}
