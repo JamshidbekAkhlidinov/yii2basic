6bc0358c506255b389c19b45117e1b8761df505a
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2023-11-29 18:30:55 +0500

    update user models

--- models/User.php
+++ models/User.php
@@ -1,104 +1,268 @@
 <?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   29 - 11 2023 17:50:55
+ *   https://ustadev.uz
+ *   https://github.com/JamshidbekAkhlidinov/yii2basic
+ */
 
 namespace app\models;
 
-class   User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use app\models\query\UserQuery;
+use yii\base\Exception;
+use yii\behaviors\AttributeBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\db\BaseActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\web\IdentityInterface;
+
+/**
+ * User model
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $email
+ * @property string $auth_key
+ * @property string $access_token
+ * @property string $oauth_client
+ * @property string $oauth_client_user_id
+ * @property string $publicIdentity
+ * @property integer $status
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $logged_at
+ * @property string $password write-only password
+ *
+ * @property \app\models\UserProfile $userProfile
+ */
+class User extends ActiveRecord implements IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
+    const STATUS_NOT_ACTIVE = 10;
+    const STATUS_ACTIVE = 20;
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
-    public static function findIdentity($id)
+    public static function tableName()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return '{{%user}}';
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::find()
+            ->active()
+            ->andWhere(['id' => $id])
+            ->one();
+    }
+
+    /**
+     * @return UserQuery
+     */
+    public static function find()
+    {
+        return new UserQuery(get_called_class());
+    }
+
+    /**
+     * @inheritdoc
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::find()
+            ->active()
+            ->andWhere(['access_token' => $token])
+            ->one();
     }
 
     /**
      * Finds user by username
      *
      * @param string $username
-     * @return static|null
+     * @return User|array|null
      */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return static::find()
+            ->active()
+            ->andWhere(['username' => $username])
+            ->one();
     }
 
     /**
-     * {@inheritdoc}
+     * Finds user by username or email
+     *
+     * @param string $login
+     * @return User|array|null
      */
-    public function getId()
+    public static function findByLogin($login)
     {
-        return $this->id;
+        return static::find()
+            ->active()
+            ->andWhere(['or', ['username' => $login], ['email' => $login]])
+            ->one();
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
+     * @throws Exception
      */
-    public function getAuthKey()
+    public function behaviors()
     {
-        return $this->authKey;
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'value' => date('Y-m-d H:i:s'),
+            ],
+            'auth_key' => [
+                'class' => AttributeBehavior::class,
+                'attributes' => [
+                    BaseActiveRecord::EVENT_BEFORE_INSERT => 'auth_key'
+                ],
+                'value' => security()->generateRandomString()
+            ],
+            'access_token' => [
+                'class' => AttributeBehavior::class,
+                'attributes' => [
+                    BaseActiveRecord::EVENT_BEFORE_INSERT => 'access_token'
+                ],
+                'value' => function () {
+                    return security()->generateRandomString(40);
+                }
+            ],
+        ];
     }
 
     /**
-     * {@inheritdoc}
+     * @return array
+     */
+    public function scenarios()
+    {
+        return ArrayHelper::merge(
+            parent::scenarios(),
+            [
+                'oauth_create' => [
+                    'oauth_client', 'oauth_client_user_id', 'email', 'username', '!status'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'email'], 'unique'],
+            ['status', 'default', 'value' => self::STATUS_NOT_ACTIVE],
+            ['status', 'in', 'range' => array_keys(self::statuses())],
+            [['username'], 'filter', 'filter' => '\yii\helpers\Html::encode']
+        ];
+    }
+
+    /**
+     * Returns user statuses list
+     * @return array|mixed
+     */
+    public static function statuses()
+    {
+        return [
+            self::STATUS_NOT_ACTIVE => translate('Not Active'),
+            self::STATUS_ACTIVE => translate('Active'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'username' => translate('Username'),
+            'email' => translate('E-mail'),
+            'status' => translate('Status'),
+            'access_token' => translate('API access token'),
+            'created_at' => translate('Created at'),
+            'updated_at' => translate('Updated at'),
+            'logged_at' => translate('Last login'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserProfile(): \yii\db\ActiveQuery
+    {
+        return $this->hasOne(UserProfile::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * @inheritdoc
      */
     public function validateAuthKey($authKey)
     {
-        return $this->authKey === $authKey;
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
     }
 
     /**
      * Validates password
-     *
      * @param string $password password to validate
-     * @return bool if password provided is valid for current user
+     * @return boolean if password provided is valid for current user
      */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return security()->validatePassword($password, $this->password_hash);
     }
+
+    /**
+     * Generates password hash from password and sets it to the model
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = security()->generatePasswordHash($password);
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicIdentity()
+    {
+        if ($this->userProfile && $this->userProfile->getFullname()) {
+            return $this->userProfile->getFullname();
+        }
+        if ($this->username) {
+            return $this->username;
+        }
+        return $this->email;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+
+    public function role()
+    {
+        $roles = authManager()->getRolesByUser($this->id);
+        return array_keys($roles)[0] ?? 'Guest';
+    }
+
 }
