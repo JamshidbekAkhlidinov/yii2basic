5164569ef847ed1efa64cd77559b5117ca394f9d
ustadev.uz MacBook <jamshidbekakhlidinov@gmail.com>
2023-12-29 16:50:12 +0500

    add env new varibles. debug and enviroment

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../config/function.php';
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', env("YII_DEBUG"));
+defined('YII_ENV') or define('YII_ENV', env("YII_ENV"));
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
