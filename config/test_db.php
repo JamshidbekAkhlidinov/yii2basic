7608c9c48ed597c55b3f6d66bd51399d0941b4ab
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2023-12-30 23:24:19 +0500

    fix

--- config/test_db.php
+++ config/test_db.php
@@ -1,5 +1,11 @@
 <?php
 $db = require __DIR__ . '/db.php';
+
+
+if (file_exists(__DIR__ . '/db_locale.php')) {
+    $db = require __DIR__ . '/db_locale.php';
+}
+
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
 
--- web/index-test.php
+++ web/index-test.php
@@ -5,10 +5,16 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
 }
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
-
+require __DIR__ . '/../config/function.php';
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', env("YII_DEBUG"));
+defined('YII_ENV') or define('YII_ENV', env("YII_ENV"));
+
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/test.php';
