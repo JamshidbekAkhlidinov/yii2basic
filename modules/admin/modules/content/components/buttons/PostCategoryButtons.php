ce35a672a52aac80a6ee2020c7f3b7adfa19ca64
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2023-12-30 23:08:48 +0500

    create post category buttons modal

---
+++ modules/admin/modules/content/components/buttons/PostCategoryButtons.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   30 - 12 2023 22:37:35
+ *   https://ustadev.uz
+ *   https://github.com/JamshidbekAkhlidinov
+ */
+
+namespace app\modules\admin\modules\content\components\buttons;
+
+use app\modules\admin\widgets\modal\ModalWidget;
+
+class PostCategoryButtons
+{
+
+    public static function create()
+    {
+        return ModalWidget::widget([
+            'button' => [
+                'tag' => 'button',
+                'class' => 'btn btn-success',
+                'label' => icon('folder-add'),
+            ],
+            'url' => ['post-category/create'],
+            'footer' => '',
+            'header' => "<h2>" . translate("Post Category Form") . "</h2>"
+        ]);
+    }
+
+    public static function update($id, $text)
+    {
+        return ModalWidget::widget([
+            'button' => [
+                'tag' => 'span',
+                'class' => '',
+                'label' => $text,
+                'options' => [
+                    'style' => [
+                        'cursor' => 'pointer'
+                    ]
+                ]
+            ],
+            'url' => ['post-category/update', 'id' => $id],
+            'footer' => '',
+            'header' => "<h2>" . translate("Post Category Form") . "</h2>"
+        ]);
+    }
+
+
+}
