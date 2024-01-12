58affd7ea4d18a6a9a73a75bb425744c72054e34
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2024-01-12 19:43:19 +0500

    create save alert trait

---
+++ traits/SaveStatusAlertTrait.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   12 - 1 2024 18:32:16
+ *   https://ustadev.uz
+ *   https://github.com/JamshidbekAkhlidinov/yii2basic
+ */
+
+namespace app\traits;
+
+trait SaveStatusAlertTrait
+{
+    protected function registerSaveStatusTrait(bool $isSaved, string $okMessage, string $failMessage)
+    {
+        if ($isSaved) {
+            session()->setFlash('alert', [
+                'options' => ['class' => 'alert-success'],
+                'body' => $okMessage
+            ]);
+        } else {
+            session()->setFlash('alert', [
+                'options' => ['class' => 'alert-danger'],
+                'body' => $failMessage
+            ]);
+        }
+    }
+}
