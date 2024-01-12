c5a829c36c7af6813748560d59a8eb3930a320a2
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2024-01-12 19:55:17 +0500

    landing element base element created

---
+++ modules/admin/modules/landingElement/base/BaseController.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   12 - 1 2024 19:37:40
+ *   https://ustadev.uz
+ *   https://github.com/JamshidbekAkhlidinov/yii2basic
+ */
+
+namespace app\modules\admin\modules\landingElement\base;
+
+use app\traits\SaveStatusAlertTrait;
+use yii\bootstrap5\Html;
+use yii\web\Controller;
+
+class BaseController extends Controller
+{
+    use SaveStatusAlertTrait;
+
+    public function return(LandingModel $form, $filename = 'form', $modelName = 'formModel')
+    {
+        if ($form->load(post())) {
+            $this->registerSaveStatusTrait(
+                $form->save(),
+                translate('Saved'),
+                Html::errorSummary($form)
+            );
+        }
+        return $this->render($filename, [
+            $modelName => $form
+        ]);
+    }
+}
--- modules/admin/modules/landingElement/base/LandingModel.php
+++ modules/admin/modules/landingElement/base/LandingModel.php
@@ -11,22 +11,110 @@ namespace app\modules\admin\modules\landingElement\base;
 
 use app\modules\admin\modules\landingElement\models\LandingElement;
 
-class Model extends \yii\base\Model
+abstract class LandingModel extends \yii\base\Model
 {
-    public LandingElement $model;
+    public const TYPE_STRING = 'string';
 
-    public function __construct(LandingElement $model, $config = [])
+    public const TYPE_PHOTO = 'photo';
+
+    public const TYPE_ARRAY = 'array';
+
+    private array $modelState = [];
+
+    public function init()
     {
-        $this->model = $model;
-        parent::__construct($config);
+        parent::init();
+        $this->loadAttributes();
+    }
+
+    public function loadAttributes()
+    {
+        foreach ($this->dataRules() as $rule) {
+            $model = $this->getModel($rule['key'] ?? []);
+            foreach ($rule['attributes'] as $thisAttribute => $modelAttribute) {
+                if ($rule['type'] == self::TYPE_STRING) {
+                    $this->{$thisAttribute} = $model->{$modelAttribute};
+                } elseif ($rule['type'] == self::TYPE_ARRAY) {
+                    $this->{$thisAttribute} = json_decode(
+                        $model->{$modelAttribute},
+                        true
+                    );
+                } else {
+                    if (!$model->isNewRecord && $model->files) {
+                        $this->{$thisAttribute} = json_decode(
+                            $model->{$modelAttribute},
+                            true
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    public function dataRules(): array
+    {
+        return [];
+    }
+
+    protected function getModel($params): LandingElement
+    {
+        $key = json_encode($params);
+        if (!isset($this->modelState[$key])) {
+            $model = LandingElement::findOne($params);
+            if ($model == null) {
+                $model = new LandingElement($params);
+            }
+            $this->modelState[$key] = $model;
+        }
+        return $this->modelState[$key];
     }
 
 
-
-    public function save()
+    public function beforeSave(): bool
     {
-        return true;
+        return $this->validate();
     }
 
+    public function save(): bool
+    {
+        if ($this->beforeSave()) {
+            return $this->saveAttributes();
+        }
+        return false;
+    }
+
+
+    public function saveAttributes(): bool
+    {
+        $transaction = app()->db->beginTransaction();
+        $isSave = true;
+        try {
+            foreach ($this->dataRules() as $rule) {
+                $model = $this->getModel($rule['key'] ?? []);
+                foreach ($rule['attributes'] as $thisAttribute => $modelAttribute) {
+                    if ($rule['type'] == self::TYPE_STRING) {
+                        $model->{$modelAttribute} = $this->{$thisAttribute};
+                    } elseif ($rule['type'] == self::TYPE_ARRAY) {
+                        $model->{$modelAttribute} = json_encode(
+                            $this->{$thisAttribute},
+                            JSON_UNESCAPED_UNICODE
+                        );
+                    } elseif ($rule['type'] == self::TYPE_PHOTO) {
+                        $model->files = $this->{$thisAttribute};
+                    } else {
+                        $model->{$modelAttribute} = $this->{$thisAttribute};
+                    }
+                }
+                $isSave = $isSave && $model->save(false);
+            }
+            if ($isSave) {
+                $transaction->commit();
+            }
+        } catch (\Throwable $exception) {
+            dd($exception->getMessage());
+            $transaction->rollBack();
+        }
+        return $isSave;
+    }
 
 }
