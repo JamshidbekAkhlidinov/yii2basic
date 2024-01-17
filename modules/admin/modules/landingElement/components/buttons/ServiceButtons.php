3d192893df4f5048d100241b4a5558d70e78f5eb
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2024-01-17 17:07:49 +0500

    button fix

--- modules/admin/modules/landingElement/components/buttons/ServiceButtons.php
+++ modules/admin/modules/landingElement/components/buttons/ServiceButtons.php
@@ -8,18 +8,18 @@
 
 namespace app\modules\admin\modules\landingElement\components\buttons;
 
+use app\modules\admin\modules\landingElement\models\LandingElement;
 use app\modules\admin\widgets\modal\ModalWidget;
 
 class ServiceButtons
 {
-
     public static function create()
     {
         return ModalWidget::widget([
             'button' => [
                 'tag' => 'button',
                 'class' => 'btn btn-success',
-                'label' => icon('folder-add'),
+                'label' => icon('fa-plus', ['icon' => 'fa']),
             ],
             'url' => ['service/create'],
             'footer' => '',
@@ -27,20 +27,20 @@ class ServiceButtons
         ]);
     }
 
-    public static function update($text)
+    public static function update(LandingElement $model)
     {
         return ModalWidget::widget([
             'button' => [
                 'tag' => 'span',
                 'class' => '',
-                'label' => $text,
+                'label' => $model->title,
                 'options' => [
                     'style' => [
                         'cursor' => 'pointer'
                     ]
                 ]
             ],
-            'url' => ['service/update', 'name' => $text],
+            'url' => ['service/update', 'id' => $model->id],
             'footer' => '',
             'header' => "<h2>" . translate("Service Update Form") . "</h2>"
         ]);
