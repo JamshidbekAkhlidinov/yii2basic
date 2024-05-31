76fc7fe2cfb8ae4df233f75ae1f1b1c2b27c5fd2
linux Jamshidbek Akhlidinov <JamshidbekAkhlidinov@gmail.com>
2024-05-31 20:53:01 +0500

    clear code

--- modules/restapi/modules/v1/resources/PostCategoryResource.php
+++ modules/restapi/modules/v1/resources/PostCategoryResource.php
@@ -11,12 +11,6 @@ namespace app\modules\restapi\modules\v1\resources;
 use app\modules\admin\modules\content\models\PostCategory;
 
 
-/**
- * @SWG\Definition(required={"name"})
- *
- * @SWG\Property(property="name", type="string")
- * @SWG\Property(property="image", type="string")
- */
 class PostCategoryResource extends PostCategory
 {
     public function fields()
